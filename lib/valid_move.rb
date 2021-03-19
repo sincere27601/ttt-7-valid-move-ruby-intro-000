9002d00447f3019fc301409679591e502a776105
Michael Raysor <michaelraysor1@gmail.com>
2021-03-19 01:04:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board,index)
-if board[index] == "x" || board[index] == "O" || 
+if board[index] == "x" || board[index] == "O" ||
    false
 elsif board[index] == " " || 
    true
